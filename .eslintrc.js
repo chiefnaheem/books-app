28fa9a8daa7738973ac1281d7a1fe1d50578630a
naheem adedokun <nadedokun@heckerbella.com>
2022-09-30 07:00:32 +0100

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["books-app", "plugin:jest/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "no-underscore-dangle": "off",
+    "no-var": "off",
+    camelcase: "off",
+    "func-names": "off",
+    "import/order": "off",
+    "import/prefer-default-export": "off",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+    jest: {
+      version: 26,
+    },
+  },
+};
