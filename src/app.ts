3f9ae8070c04aee966a226582b633da4c711f1f9
naheem adedokun <nadedokun@heckerbella.com>
2022-09-30 15:35:11 +0100

    push updates

--- src/app.ts
+++ src/app.ts
@@ -16,13 +16,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
-
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'jade');
-
 
 //Routes
 app.use('/api/v1', router);
@@ -30,18 +28,7 @@ app.use('/api/v1', router);
 
 app.use("/testing", (req: Request, res: Response, next: NextFunction) => {
   res.send('Hi there')
-})
-
-// console.log(process.env.NODE_ENV)
-// if (process.env.NODE_ENV === 'test') {
-//   connectTestDB()
-// }
-// else{
-//   connectDB()
-// }
-// app.get('/test', (req: any,res: any)=>{
-//   res.send({data:'api working'})
-// })
+});
 
 // catch 404 and forward to error handler
 app.use(function(req:Request, res:Response, next:NextFunction) {
